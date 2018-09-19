08c6e762158117ea4940e16632883c58f6e84520
guoben <1554710628@qq.com>
2018-09-19 12:04:03 +0800

    第一次提交

---
+++ app/src/main/java/com/example/administrator/mybuildmode/Builder.java
@@ -0,0 +1,15 @@
+package com.example.administrator.mybuildmode;
+
+public interface Builder {
+
+    public Builder buildHeader();
+
+    public Builder buildBody();
+
+    public Builder buildHands();
+
+    public Builder buildFooter();
+
+    public Person result();
+
+}
